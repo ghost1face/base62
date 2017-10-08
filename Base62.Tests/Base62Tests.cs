82e5455df681578686187d438135073cdc8cade9
Daniel Destouche <daniel.destouche@gmail.com>
2017-10-08 17:04:18 -0400

    Update unit tests

--- Base62.Tests/Base62Tests.cs
+++ Base62.Tests/Base62Tests.cs
@@ -9,6 +9,18 @@ namespace Base62.Tests
         public void Encoded_CanBe_Decoded()
         {
             var input = "120";
+            var converter = new Base62Converter();
+            var encoded = converter.Encode(input);
+
+            var decoded = converter.Decode(encoded);
+
+            Assert.AreEqual(input, decoded);
+        }
+
+        [TestMethod]
+        public void Encoded_Inverted_CanBe_Decoded()
+        {
+            var input = "Whatup";
             var converter = new Base62Converter(Base62Converter.CharacterSet.INVERTED);
             var encoded = converter.Encode(input);
 
