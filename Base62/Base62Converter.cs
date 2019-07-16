42171161555503d6f93e792bdb2366370d55debc
Daniel Destouche <daniel.destouche@gmail.com>
2019-07-16 02:07:30 -0400

    Update Base62Converter to support UTF8 character set

--- Base62/Base62Converter.cs
+++ Base62/Base62Converter.cs
@@ -24,27 +24,23 @@ namespace Base62
 
         public string Encode(string value)
         {
-            var arr = new int[value.Length];
-            for (var i = 0; i < arr.Length; i++)
-            {
-                arr[i] = value[i];
-            }
+            var arr = Encoding.UTF8.GetBytes(value);
 
             return Encode(arr);
         }
 
         public string Decode(string value)
         {
-            var arr = new int[value.Length];
+            var arr = new byte[value.Length];
             for (var i = 0; i < arr.Length; i++)
             {
-                arr[i] = characterSet.IndexOf(value[i]);
+                arr[i] = (byte)characterSet.IndexOf(value[i]);
             }
 
             return Decode(arr);
         }
 
-        private string Encode(int[] value)
+        private string Encode(byte[] value)
         {
             var converted = BaseConvert(value, 256, 62);
             var builder = new StringBuilder();
@@ -55,31 +51,27 @@ namespace Base62
             return builder.ToString();
         }
 
-        private string Decode(int[] value)
+        private string Decode(byte[] value)
         {
             var converted = BaseConvert(value, 62, 256);
-            var builder = new StringBuilder();
-            for (var i = 0; i < converted.Length; i++)
-            {
-                builder.Append((char)converted[i]);
-            }
-            return builder.ToString();
+
+            return Encoding.UTF8.GetString(converted, 0, converted.Length);
         }
 
-        private static int[] BaseConvert(int[] source, int sourceBase, int targetBase)
+        private static byte[] BaseConvert(byte[] source, int sourceBase, int targetBase)
         {
             var result = new List<int>();
             int count = 0;
             while ((count = source.Length) > 0)
             {
-                var quotient = new List<int>();
+                var quotient = new List<byte>();
                 int remainder = 0;
                 for (var i = 0; i != count; i++)
                 {
                     int accumulator = source[i] + remainder * sourceBase;
-                    int digit = accumulator / targetBase;
+                    byte digit = System.Convert.ToByte((accumulator - (accumulator % targetBase)) / targetBase);
                     remainder = accumulator % targetBase;
-                    if (quotient.Count > 0 || digit > 0)
+                    if (quotient.Count > 0 || digit != 0)
                     {
                         quotient.Add(digit);
                     }
@@ -89,7 +81,11 @@ namespace Base62
                 source = quotient.ToArray();
             }
 
-            return result.ToArray();
+            var output = new byte[result.Count];
+            for (int i = 0; i < result.Count; i++)
+                output[i] = (byte)result[i];
+
+            return output;
         }
 
         public enum CharacterSet
